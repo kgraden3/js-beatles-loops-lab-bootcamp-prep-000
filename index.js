3d48bd009d6dcb2c8f9f6fb19350f7f2a68c7c9e
Kyle Graden <kylegraden3@gmail.com>
2019-02-13 20:31:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // add solution here
+
+
+function theBeatlesPlay(musician, instrument){
+  var theBeatles = []
+  for (let i = 0; i++ ) {
+    return theBeatles.push(`${musician} plays ${instrument}`)
+  }
+  return theBeatles
+}
